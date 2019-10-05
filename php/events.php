f6c9975ed3468223373d8b8ce1ca5c9dfb27ceae
AbhishekAjoy <abhiajoy23@gmail.com>
2019-10-05 18:22:13 +0530

    051019

--- php/events.php
+++ php/events.php
@@ -1,15 +1,77 @@
 <html>
 <head>
 <link href="../css/theme.css" rel="stylesheet">
+<link href="../css/events.css" rel="stylesheet">
 
 </head>
 <body>
 <div class="topnav">
-  <a  href="../php/home.php">Home</a>
+  <a href="../php/home.php">Home</a>
   <a class="active" href="../php/events.php">Events</a>
   <a href="../php/points.php">Points Table</a>
   <a href="../php/results.php">Results</a>
 </div> 
-<center><h1>Events</h1></center><br>
+<br>
+<div class = "note">
+  <center>Create an account and login to register for the events</center>
+</div>
+<br><br>
+<div class="Workshops">
+  <h2>WORKSHOPS</h2>
+  <div class="imagebox">
+  <div class="row">
+    <div class="column">
+    <img src="../images/deepl.jpeg" alt="img" width="200" height="200"> <!-- the image -->
+     </div>
+    <div class="column">
+    <img src="../images/selfd.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/iot.jpeg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/matlab.jpeg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/hack.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+  </div>
+  </div>
+   <br>
+    Self driving cars
+    IoT
+    Matlab and Simulink
+    Ethical Hacking
+</div>
+<br><br><br>
+<div class = "Competitions">
+<h2>COMPETITIONS</h2>
+<div class="imagebox">
+  <div class="row">
+    <div class="column">
+    <img src="../images/treasure.jpg" alt="img" width="200" height="200"> <!-- the image -->
+     </div>
+    <div class="column">
+    <img src="../images/robo.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/quiz.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/drone.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+    <div class="column">
+    <img src="../images/code.jpg" alt="img" width="200" height="200"> <!-- the image -->
+    </div>
+  </div>
+  </div>
+  <ol>
+    <li>Treasure Hunt</li>
+    <li>Robo Wars</li>
+    <li>Tech Trivia</li>
+    <li>Drone Racing</li>
+    <li>Code King</li>
+  </ol>
+</div>
 </body>
 </html>
